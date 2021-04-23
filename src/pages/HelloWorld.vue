6b7d5896f8179bb8251d82e70fe2e5a9bcd15031
zhangtch <zhangtch@yonyou.com>
2021-04-23 14:57:19 +0800

    yodate

--- src/pages/HelloWorld.vue
+++ src/pages/HelloWorld.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="hello">
+    <a-button type="primary">
+      Primary
+    </a-button>
+    <a-button>Default</a-button>
+    <a-button type="dashed">
+      Dashed
+    </a-button>
+    <a-button type="danger">
+      Danger
+    </a-button>
+    <a-config-provider :auto-insert-space-in-button="false">
+      <a-button type="primary">
+        按钮
+      </a-button>
+    </a-config-provider>
+    <a-button type="primary">
+      按钮
+    </a-button>
+    <a-button type="link">
+      Link
+    </a-button>
     <h1>{{ msg }}</h1>
     <h2 v-on:click="add">Essential Links</h2>
     {{JSON.stringify(this.userState.count)}}
